3dea1da09f28ef372ebf1fc6a36c68c5b2d94a72
Furkan Boncuk <114020260+Furkan-Boncuk@users.noreply.github.com>
2022-12-19 11:44:14 +0300

    Update Main.java
    Since the directories here are the ones on my computer, it may be useless for you to review these codes, sorry for that.

--- Main.java
+++ Main.java
@@ -5,44 +5,44 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        File dosya = new File("C:\\__Java_PatikaDev\\Input-Output_Islemleri\\_01_io\\src\\_02\\patika.txt");
+        File file = new File("C:\\__Java_PatikaDev\\Input-Output_Islemleri\\_01_io\\src\\_02\\patika.txt");
 
-        //DOSYA OLUŞTUR----------------------------------------------------------------------------------------------------------------
+        //create file-----------------------------------------------------------------------------------------------------------
         try{
-            if(dosya.createNewFile()){
-                System.out.println(dosya.getName()+" dosyası oluşturuldu");
+            if(file.createNewFile()){
+                System.out.println(file.getName()+" file created");
             }else{
-                System.out.println(dosya.getName()+" dosyası zaten mevcut !");
+                System.out.println(file.getName()+" file already exists");
             }
         }catch (IOException e) {
             System.out.println(e.getMessage());
         }
 
-        //DOSYA SİL---------------------------------------------------------------------------------------------------------------------
-        dosya.delete();
+        //delete file------------------------------------------------------------------------------------------------------------
+        file.delete();
 
-        //VAR OLMAYAN BİR DİZİN ÜZERİNE DOSYA OLUŞTURMAK--------------------------------------------------------------------------------
-        File dizin = new File("C:\\__Java_PatikaDev\\Input-Output_Islemleri\\_01_io\\src\\_02\\test");
-        dizin.mkdir();
-        File dosya2 = new File("C:\\__Java_PatikaDev\\Input-Output_Islemleri\\_01_io\\src\\_02\\test\\patika.txt");
+        //create a file on a non-existing directory------------------------------------------------------------------------------
+        File directory = new File("C:\\__Java_PatikaDev\\Input-Output_Islemleri\\_01_io\\src\\_02\\test");
+        directory.mkdir();
+        File file2 = new File("C:\\__Java_PatikaDev\\Input-Output_Islemleri\\_01_io\\src\\_02\\test\\patika.txt");
         try{
-            if(dosya2.createNewFile()){
-                System.out.println(dosya2.getName()+" dosyası "+dizin.getName()+" dizininin altına oluşturuldu");
+            if(file2.createNewFile()){
+                System.out.println(file2.getName()+" dosyası "+directory.getName()+" dizininin altına oluşturuldu");
             }else{
-                System.out.println(dosya2.getName()+" dosyası zaten mevcut !");
+                System.out.println(file2.getName()+" dosyası zaten mevcut !");
             }
         }catch(IOException e){
             System.out.println(e.getMessage());
         }
 
-        //YALNIZCA DİZİNLERİ OLUŞTURMAK-------------------------------------------------------------------------------------------------
-        File dizin2 = new File("Patika/dev");
-        System.out.println(dizin2.mkdirs());  //!!! Bu durumda mkdirs() fonksiyonu kullanılır.
+        //only create directories-------------------------------------------------------------------------------------------------
+        File directory2 = new File("Patika/dev");
+        System.out.println(directory2.mkdirs());  //In this case, the mkdirs() function is used.
 
-        //DİZİNDEKİ DOSYALARI LİSTELEMEK------------------------------------------------------------------------------------------------
-        File dizin3 = new File("Patika");
-        String[] liste = dizin3.list();
-        for(String str : liste){
+        //list files in directory-------------------------------------------------------------------------------------------------
+        File directory3 = new File("Patika");
+        String[] list1 = directory3.list();
+        for(String str : list1){
             System.out.println(str);
         }
     }
